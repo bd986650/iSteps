18bd981450d9f0ce3e5749b2a15569b09829ecda
qwotic <qwoticreator@gmail.com>
2023-07-04 18:27:40 +0700

    Initial Commit

---
+++ iSteps/iStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStepsApp.swift
+//  iSteps
+//
+//  Created by Данил Белов on 04.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
